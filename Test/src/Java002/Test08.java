5f071797f6c31564f36eebe6cabf812263d90b83
Asami Tsuyuki <>
2016-07-23 16:49:24 +0900

    Test

--- Test/src/Java002/Test08.java
+++ Test/src/Java002/Test08.java
@@ -6,15 +6,15 @@ package Java002;
 
 /**
  * @author （作成者）a-tsuyuki <br />
- *         （クラス論理名）Test07 <br />
- *         （説明）1~100までの数値をループで回して、加算する。 <br />
+ *         （クラス論理名）Test08 <br />
+ *         （説明）1~100までの数値を奇数の時のみ加算。 <br />
  *         更新履歴 2016/07/23 （更新者）：（説明） <br />
  */
-public class Test07 {
+public class Test08 {
 
   /**
    * （メソッド論理名）main <br />
-   * （説明）1~100までの数値をループで回して、加算する。 <br />
+   * （説明）1~100までの数値を奇数の時のみ加算。 <br />
    * 
    * @param args 実行時引数
    */
@@ -23,10 +23,12 @@ public class Test07 {
 
     int j = 0;
     for ( int i = 0; i < 101; i++ ) {
-      j = j + i;
-      // System.out.println(i);
+      if ( i % 2 != 0 ) {
+        j = j + i;
+      }
+//      System.out.println( i );
     }
-    System.out.println( "1~100を加算した数値を表示します。 " );
+    System.out.println( "1~100の奇数を加算した数値を表示します。 " );
     System.out.print( "合計：" );
     System.out.println( j );
 
--- Test/src/Java002/Test10.java
+++ Test/src/Java002/Test10.java
@@ -6,27 +6,31 @@ package Java002;
 
 /**
  * @author （作成者）a-tsuyuki <br />
- *         （クラス論理名）Test07 <br />
- *         （説明）1~100までの数値をループで回して、加算する。 <br />
+ *         （クラス論理名）Test10 <br />
+ *         （説明）「実行時引数で渡された数値」までをループで回して、加算する。 <br />
  *         更新履歴 2016/07/23 （更新者）：（説明） <br />
  */
-public class Test07 {
+public class Test10 {
 
   /**
    * （メソッド論理名）main <br />
-   * （説明）1~100までの数値をループで回して、加算する。 <br />
+   * （説明）「実行時引数で渡された数値」までをループで回して、加算する。 <br />
    * 
    * @param args 実行時引数
    */
 
   public static void main( String[] args ) {
 
+    String x = args[0];
+    int y = Integer.parseInt( x );
     int j = 0;
-    for ( int i = 0; i < 101; i++ ) {
+    for ( int i = 0; i < y + 1; i++ ) {
       j = j + i;
       // System.out.println(i);
     }
-    System.out.println( "1~100を加算した数値を表示します。 " );
+    System.out.print( "1~" );
+    System.out.print( y );
+    System.out.println( "まで加算した数値を表示します。 " );
     System.out.print( "合計：" );
     System.out.println( j );
 
