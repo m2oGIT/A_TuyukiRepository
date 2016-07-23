9ed10d0d3ee2d7ef7041e7641a9362ad4533ecdd
Asami Tsuyuki <>
2016-07-23 15:49:04 +0900

    Test005

---
+++ Test/src/Java001/Test05.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright 2016 EIS Co., Ltd. All rights reserved.
+ */
+package Java001;
+
+/**
+ * @author （a-tsuyuki） <br />
+ *  （Test05） <br />
+ *  （説明）複数の文字列を起動時引数に渡して、コンソールに表示 <br />
+ *  更新履歴 2016/07/23 （更新者）：（説明） <br />
+ */
+public class Test05 {
+
+  /**
+   * （メソッド論理名） main<br />
+   * （説明）複数の文字列を起動時引数に渡して、コンソールに表示 <br />
+   * @param args 複数の引数を表示
+   */
+  public static void main( String[] args ) {
+    System.out.println(args[0]);
+    System.out.println(args[1]);
+  }
+
+}
